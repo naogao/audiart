8f9401ae039b3a793e00d97b5d4c533084b7909e
katherine hu <hu.z.katherine@gmail.com>
2020-12-17 13:56:37 -0500

    got image t show up from p5

--- js/sketch.js
+++ js/sketch.js
@@ -161,29 +161,79 @@ function getCurrentTrack() {
 
 let song, analyzer;
 
-function preload() {
-  song = loadSound('assets/claude.mp3');
-}
+// function preload() {
+//   song = loadSound('assets/claude.mp3');
+// }
+
+// function setup() {
+//   createCanvas(710, 200);
+//   song.loop();
+
+//   // create a new Amplitude analyzer
+//   analyzer = new p5.Amplitude();
+
+//   // Patch the input to an volume analyzer
+//   analyzer.setInput(song);
+// }
+
+// function draw() {
+//   background(255);
+
+//   // Get the average (root mean square) amplitude
+//   let rms = analyzer.getLevel();
+//   fill(127);
+//   stroke(0);
+
+//   // Draw an ellipse with size based on volume
+//   ellipse(width / 2, height / 2, 10 + rms * 200, 10 + rms * 200);
+// }
+
+// By Roni Kaufman
+
+let kMax;
+let step;
+let n = 250; // number of blobs
+let radius = 80; // diameter of the circle
+let inter = 0.05; // difference between the sizes of two blobs
+let maxNoise = 500;
+
+let noiseProg = (x) => (x*x);
 
 function setup() {
-  createCanvas(710, 200);
-  song.loop();
-
-  // create a new Amplitude analyzer
-  analyzer = new p5.Amplitude();
-
-  // Patch the input to an volume analyzer
-  analyzer.setInput(song);
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB, 1);
+  angleMode(DEGREES);
+  noFill();
+  noLoop();
+  kMax = random(0.5, 1);
+  step = 0.01;
 }
 
 function draw() {
-  background(255);
+  background(0.1);
+  
+  for (let i = 0; i < n; i++) {
+    let alpha = 1 - noiseProg(i / n);
+    stroke(0.9, alpha);
+    let size = radius + i * inter;
+    let k = kMax * sqrt(i/n);
+    let noisiness = maxNoise * noiseProg(i / n);
+    blob(size, width/2, height/2, k, i * step, noisiness);
+  }
+}
 
-  // Get the average (root mean square) amplitude
-  let rms = analyzer.getLevel();
-  fill(127);
-  stroke(0);
-
-  // Draw an ellipse with size based on volume
-  ellipse(width / 2, height / 2, 10 + rms * 200, 10 + rms * 200);
+function blob(size, xCenter, yCenter, k, t, noisiness) {
+  beginShape();
+  let angleStep = 360 / 500;
+  for (let theta = 0; theta < 360; theta += angleStep) {
+    let r1, r2;
+    
+    r1 = cos(theta)+1;
+    r2 = sin(theta)+1;
+    let r = size + noise(k * r1,  k * r2, t) * noisiness;
+    let x = xCenter + r * cos(theta);
+    let y = yCenter + r * sin(theta);
+    curveVertex(x, y);
+  }
+  endShape(CLOSE);
 }
