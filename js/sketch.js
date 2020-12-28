553c4ddabc1f6feed0590d0bda22f2f20f49b8d7
Naomie Gao <naomie.gao@duke.edu>
2020-12-28 12:36:06 -0500

    testing search tracks

--- js/sketch.js
+++ js/sketch.js
@@ -116,13 +116,14 @@ function searchForSong(myInput) {
          //  Display the song name and artist on the page
          document.getElementById("name").innerHTML = searchList.tracks.items[0].name
          document.getElementById("artist").innerHTML = searchList.tracks.items[0].artists[0].name
-
+		 document.getElementById("test").innerHTML = searchList.tracks.items[0].name
          //  Using that track's ID, grab the Audio Features
 		 searchAudioFeatures(songID)
 		 searchAudioAnalysis();
          //  Clear the search box
-         document.getElementById("searchBox").value = "test2";
-         myInput = "test"
+         document.getElementById("searchBox").value = "";
+		 myInput = ""
+		 
        }
      }
    })
